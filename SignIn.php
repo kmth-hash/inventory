50d9513b1b56c655d602cfa7a30edee200ae53d2
kmth-hash <bpk.san4ya@gmail.com>
2021-11-18 13:59:31 +0530

    partial mainpage complete

---
+++ SignIn.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/css/styles.css" rel="stylesheet">
+</head>
+<body>
+<section class="h-100 gradient-form" style="background-color: #eee;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-xl-10 index_row">
+        <div class="card rounded-3 text-black">
+          <div class="row g-0" style="background-color : #f9fafc">
+            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                <div class="text-black px-3 py-4 p-md-5 mx-md-4">
+                  <img src="./assets/img/SignUp.jpg" alt="">
+                  
+                </div>
+              </div>
+            <div class="col-lg-6">
+              <div class="card-body p-md-5 mx-md-4">
+                
+                <form>
+                  <p> </p>
+
+                  <div class="form-outline mb-4">
+                    <label class="form-label" for="form2Example11">Username</label>
+                    <input type="email" id="form2Example11" class="form-control" placeholder="Phone number or email address"/>                    
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <label class="form-label" for="form2Example22">Password</label>
+                    <input type="password" id="form2Example22" class="form-control" />                    
+                  </div>
+
+                  <div class="text-center pt-1 mb-5 pb-1">
+                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button"> Log in</button>
+                    <a class="text-muted" href="#!">Forgot password?</a>
+                  </div>
+                  <!--
+                  <div class="d-flex align-items-center justify-content-center pb-4">
+                    <p class="mb-0 me-2">Don't have an account?</p>  &nbsp;
+                    <button type="button" class="btn btn-outline-danger">Create new</button>
+                  </div>
+                  -->
+                </form>
+
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</body>
+</html>
+
